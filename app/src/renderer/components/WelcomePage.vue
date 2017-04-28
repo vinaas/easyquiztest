75e640132747344d476868b876f3e84996edcc68
quocbao-pc <trinhngocquocbao@vienthonga.com>
2017-04-28 10:50:56 +0700

    tạo mới trang Welcompage

---
+++ app/src/renderer/components/WelcomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <login></login>
+    <current-page></current-page>
+  </div>
+</template>
+
+<script>
+  
+  import CurrentPage from './LandingPageView/CurrentPage'
+  import Links from './LandingPageView/Links'
+  import Versions from './LandingPageView/Versions'
+  import Login from './LandingPageView/Login'
+  export default {
+    components: {
+      CurrentPage,
+      Links,
+      Versions,
+      Login
+    },
+    name: 'landing-page'
+  }
+</script>
+
+<style scoped>
+  img {
+    margin-top: -25px;
+    width: 450px;
+  }
+</style>
