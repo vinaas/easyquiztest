b5ac1a42c9a060b33d7cd94cf6071e57dce59aa8
quocbao-pc <trinhngocquocbao@vienthonga.com>
2017-04-28 17:04:36 +0700

    thêm 1 số tính năng

--- app/src/renderer/components/LandingPageView/Login.vue
+++ app/src/renderer/components/LandingPageView/Login.vue
@@ -2,6 +2,17 @@
  <div>
   <h3>Login page of Easy QuizTest</h3>
   <h4>User Name</h4>
-  <h4>Password</h4>
+  <div class="custom-controls-stacked">
+  <label class="custom-control custom-radio">
+    <input id="radioStacked1" name="radio-stacked" type="radio" class="custom-control-input">
+    <span class="custom-control-indicator"></span>
+    <span class="custom-control-description">Toggle this custom radio</span>
+  </label>
+  <label class="custom-control custom-radio">
+    <input id="radioStacked2" name="radio-stacked" type="radio" class="custom-control-input">
+    <span class="custom-control-indicator"></span>
+    <span class="custom-control-description">Or toggle this other custom radio</span>
+  </label>
+</div>
   </div>
 </template>
