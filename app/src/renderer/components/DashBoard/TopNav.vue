a02e4ffd49f3a808377a3cbe5d25a7199742dd9d
tungptvn <tungpt.hd@gmail.com>
2017-05-02 22:36:09 +0700

    feat(*): add dashboard

---
+++ app/src/renderer/components/DashBoard/TopNav.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">Home</a>
+    
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                     <router-link class="nav-link" to="/">dashboard</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/about">about</router-link>
+    
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/login">login</router-link>
+                </li>
+            </ul>
+            <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</template>
