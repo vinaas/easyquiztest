d642274f58127344b90b99979bda1ca5aae9ef46
tungptvn <tungpt.hd@gmail.com>
2017-05-21 21:51:46 +0700

    feat(*): add custom coundown

---
+++ app/src/common/coundown.js
@@ -0,0 +1,17 @@
+export default function (duration, display) {
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
