ccde71682281803763d8062333b8c578e30a906f
tungptvn <tungpt.hd@gmail.com>
2017-05-21 19:14:25 +0700

    feat(pack): add toastr

--- app/src/renderer/App2.vue
+++ app/src/renderer/App2.vue
@@ -10,6 +10,7 @@ import 'jquery/dist/jquery.min.js'
 import 'semantic-ui-css/semantic.css'
 import '../../node_modules/semantic-ui-css/semantic.js'
 import 'font-awesome/css/font-awesome.css'
+import '../../node_modules/toastr/build/toastr.min.css'
 import './style/style.css'
 import store from 'renderer/vuex/store'
 export default {
