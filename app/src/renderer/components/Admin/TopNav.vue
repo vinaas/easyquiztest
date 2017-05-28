64be03489bdfaccd1f53d9a7a3f090d739f1c35b
tungptvn <tungpt.hd@gmail.com>
2017-05-28 18:34:11 +0700

    feat(*): add logout right menu

--- app/src/renderer/components/Admin/TopNav.vue
+++ app/src/renderer/components/Admin/TopNav.vue
@@ -1,32 +1,41 @@
 <template>
-    <div class="ui fixed  menu">
+    <div class="ui fixed menu">
         <div class="ui container">
             <router-link class="item header item" to="/"> <img class="logo" src="../assets/vinaas-logo.png">EasyQuizTest</router-link>
             <a href="javascript:void()" class="item">Quản lý thí sinh</a>
             <a href="javascript:void()" class="item">Quản lý đề thi</a>
-            <!--<a href="/" class="header item">
-                <img class="logo" src="../assets/vinaas-logo.png">EasyQuizTest
-            </a>-->
+            <div class="ui dropdown item right">
+                admin
+                <i class="dropdown icon"></i>
+                <div class="menu">
+                    <a class="item"><i class="settings icon"></i> Cài đặt</a>
+                    <a class="item"><i class="sign out icon"></i> Đăng xuất</a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    mounted: function () {
-        $(document)
+  mounted: function () {
+    $(document)
             .ready(function () {
-                $('.ui.menu .ui.dropdown').dropdown({
-                    on: 'hover'
-                });
-                $('.ui.menu a.item')
+              $('.ui.dropdown').dropdown({
+                on: 'hover'
+              })
+              $('.ui.menu a.item')
                     .on('click', function () {
-                        $(this)
+                      $(this)
                             .addClass('active')
                             .siblings()
-                            .removeClass('active');
-                    });
-            });
-    }
+                            .removeClass('active')
+                    })
+            })
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
