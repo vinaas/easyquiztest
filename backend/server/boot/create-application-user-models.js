49836ace5e35bafd1c1b8ce4e011a4c31e43220a
tungptvn <tungpt.hd@gmail.com>
2017-05-30 13:47:07 +0700

    chore(*): comment create default user

--- backend/server/boot/create-application-user-models.js
+++ backend/server/boot/create-application-user-models.js
@@ -1,56 +1,56 @@
 module.exports = function (app) {
-  var User = app.models.ApplicationUser;
-  var Role = app.models.Role;
-  var RoleMapping = app.models.RoleMapping;
+  // var User = app.models.ApplicationUser;
+  // var Role = app.models.Role;
+  // var RoleMapping = app.models.RoleMapping;
 
-  User.create([{
-      username: 'admin1',
-      email: 'john@doe.com',
-      password: 'showmethepassword'
-    },
-    {
-      username: 'admin2',
-      email: 'jane@doe.com',
-      password: 'showmethepassword'
-    },
-    {
-      username: 'test',
-      email: 'bob@projects.com',
-      password: 'showmethepassword'
-    }
-  ], function (err, users) {
-    if (err) throw err;
+  // User.create([{
+  //     username: 'admin1',
+  //     email: 'john@doe.com',
+  //     password: 'showmethepassword'
+  //   },
+  //   {
+  //     username: 'admin2',
+  //     email: 'jane@doe.com',
+  //     password: 'showmethepassword'
+  //   },
+  //   {
+  //     username: 'test',
+  //     email: 'bob@projects.com',
+  //     password: 'showmethepassword'
+  //   }
+  // ], function (err, users) {
+  //   if (err) throw err;
 
-    console.log('Created users:', users);
+  //   console.log('Created users:', users);
 
-    // create project 1 and make john the owner
+  //   // create project 1 and make john the owner
 
 
-    //create the admin role
-    Role.create({
-      name: 'admin'
-    }, function (err, role) {
-      if (err) throw err;
+  //   //create the admin role
+  //   Role.create({
+  //     name: 'admin'
+  //   }, function (err, role) {
+  //     if (err) throw err;
 
-      console.log('Created role:', role);
+  //     console.log('Created role:', role);
 
-      //make bob an admin
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: users[2].id
-      }, function (err, principal) {
-        if (err) throw err;
+  //     //make bob an admin
+  //     role.principals.create({
+  //       principalType: RoleMapping.USER,
+  //       principalId: users[2].id
+  //     }, function (err, principal) {
+  //       if (err) throw err;
 
-        console.log('Created principal:', principal);
-      });
-    });
-  });
-  RoleMapping.belongsTo(User);
-  User.hasMany(RoleMapping, {
-    foreignKey: 'principalId'
-  });
-  Role.hasMany(User, {
-    through: RoleMapping,
-    foreignKey: 'roleId'
-  });
+  //       console.log('Created principal:', principal);
+  //     });
+  //   });
+  // });
+  // RoleMapping.belongsTo(User);
+  // User.hasMany(RoleMapping, {
+  //   foreignKey: 'principalId'
+  // });
+  // Role.hasMany(User, {
+  //   through: RoleMapping,
+  //   foreignKey: 'roleId'
+  // });
 };
