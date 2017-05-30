674bcb6426a5e5439922316cec53e6b916430e67
tungptvn <tungpt.hd@gmail.com>
2017-05-30 13:45:17 +0700

    feat(*): add admin role

--- backend/server/boot/create-application-user-models.js
+++ backend/server/boot/create-application-user-models.js
@@ -1,7 +1,56 @@
-'use strict';
+module.exports = function (app) {
+  var User = app.models.ApplicationUser;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
 
-var async = require('async');
-module.exports = function(app) {
-  //data sources
- 
+  User.create([{
+      username: 'admin1',
+      email: 'john@doe.com',
+      password: 'showmethepassword'
+    },
+    {
+      username: 'admin2',
+      email: 'jane@doe.com',
+      password: 'showmethepassword'
+    },
+    {
+      username: 'test',
+      email: 'bob@projects.com',
+      password: 'showmethepassword'
+    }
+  ], function (err, users) {
+    if (err) throw err;
+
+    console.log('Created users:', users);
+
+    // create project 1 and make john the owner
+
+
+    //create the admin role
+    Role.create({
+      name: 'admin'
+    }, function (err, role) {
+      if (err) throw err;
+
+      console.log('Created role:', role);
+
+      //make bob an admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[2].id
+      }, function (err, principal) {
+        if (err) throw err;
+
+        console.log('Created principal:', principal);
+      });
+    });
+  });
+  RoleMapping.belongsTo(User);
+  User.hasMany(RoleMapping, {
+    foreignKey: 'principalId'
+  });
+  Role.hasMany(User, {
+    through: RoleMapping,
+    foreignKey: 'roleId'
+  });
 };
