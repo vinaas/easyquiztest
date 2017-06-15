871c232e7e31f0c664de7fc692a68cf1edf52be0
tungptvn <tungpt.hd@gmail.com>
2017-06-15 21:31:58 +0700

    feat(*): add AnswersForAQuestionService

---
+++ app/src/renderer/services/answers-for-aquestion.js
@@ -0,0 +1,21 @@
+import Promise from 'bluebird'
+const pathEntity = '/api/AnswersForAQuestions';
+export class AnswersForAQuestionService {
+    getAnswersForAQuestions = Promise.coroutine(function* () {
+        let ret = yield axios.get(pathEntity)
+        return ret.data
+    })
+    getAnswersForAQuestion = Promise.coroutine(function* (id) {
+        let ret = yield axios.get(`${pathEntity}/${id}`)
+        return ret.data
+    })
+    save = Promise.coroutine(function* (bindingEntity) {
+        let ret = yield axios.post(`${pathEntity}/replaceOrCreate`, bindingEntity)
+        return ret.data
+    })
+    remove = Promise.coroutine(function* (id) {
+        let ret = yield axios.delete(`${pathEntity}/${id}`)
+        return ret.data
+    })
+
+}
