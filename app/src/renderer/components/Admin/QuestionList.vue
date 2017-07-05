1c7efa5441e29064df1c06b6e8fee266afd9b79a
vvthinh <vvthinh1545@gmail.com>
2017-07-05 09:35:40 +0700

    Ghi chú:
     app/package.json
        install datatables.net,datatables.net-dt
    modules adminQuestions
     + feat datatable
     + List Questions
     + Action Cập nhật

--- app/src/renderer/components/Admin/QuestionList.vue
+++ app/src/renderer/components/Admin/QuestionList.vue
@@ -1,14 +1,79 @@
 <template>
-    <div class="ui main container">
-        <h1>
-            Questions manager</h1>
-    
-    </div>
+  <div class="ui main text container">
+
+    <table id="questions" class="display dataTable" cellspacing="0" width="100%">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Content</th>
+          <th>Description</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+
 </template>
+
 <script>
-export default {
-    created: function () {
-        console.log('this', this.$route.params)
-    }
-}
+  import 'datatables.net-dt/css/jquery.datatables.css';
+  import {
+    mapState,
+    mapGetters
+  } from 'vuex'
+  import _ from 'lodash'
+  import Promise from 'Bluebird'
+  const co = Promise.coroutine
+  var $ = require('jquery');
+  require('datatables.net')(window, $);
+
+  export default {
+    computed: {
+      ...mapState('adminQuestions', {
+        questions: state => state.all
+
+      })
+    },
+
+    created: co(function* () {
+
+      this.$store.dispatch('adminQuestions/getAll').then(() => {
+
+        $(document).ready(() => {
+
+          let me = this
+          let table = $('#questions').DataTable({
+            data: me.questions,
+
+            "columns": [{
+                "data": "id"
+              },
+              {
+                "data": "content"
+              },
+              {
+                "data": "description"
+              },
+              {
+                "data": "Action"
+              }
+
+            ],
+            "columnDefs": [{
+              "targets": -1,
+              "data": null,
+              "defaultContent": "<button>Cập nhật</button>"
+            }]
+          })
+          $('#questions tbody').on('click', 'tr', function () {
+            var data = table.row(this).data();
+            //  console.log('data', data.answersForAQuestions[0], data.id, data);
+            //alert(data.answersForAQuestions[0])
+          });
+        })
+      })
+
+    })
+
+  }
 </script>
--- app/src/renderer/services/question.js
+++ app/src/renderer/services/question.js
@@ -1,21 +1,26 @@
 import Promise from 'bluebird'
-const pathEntity = '/api/questions';
+const pathEntity = '/api/questions'
 export class QuestionService {
-    getAll = Promise.coroutine(function* () {
-        let ret = yield axios.get(pathEntity)
-        return ret.data
+  getAll = Promise.coroutine(function* () {
+    let ret = yield axios.get(pathEntity)
+    return ret.data.map(x => {
+      x.content= x.content || '';
+      x.description = x.description || '';
+      return x
     })
-    getBy = Promise.coroutine(function* (id) {
-        let ret = yield axios.get(`${pathEntity}/${id}`)
-        return ret.data
-    })
-    save = Promise.coroutine(function* (bindingEntity) {
-        let ret = yield axios.post(`${pathEntity}/replaceOrCreate`, bindingEntity)
-        return ret.data
-    })
-    remove = Promise.coroutine(function* (id) {
-        let ret = yield axios.delete(`${pathEntity}/${id}`)
-        return ret.data
-    })
+  })
+  getBy = Promise.coroutine(function* (id) {
+    let ret = yield axios.get(`${pathEntity}/${id}`)
+
+    return ret.data
+  })
+  save = Promise.coroutine(function* (bindingEntity) {
+    let ret = yield axios.post(`${pathEntity}/replaceOrCreate`, bindingEntity)
+    return ret.data
+  })
+  remove = Promise.coroutine(function* (id) {
+    let ret = yield axios.delete(`${pathEntity}/${id}`)
+    return ret.data
+  })
 
 }
