06eab2232f544e612fcb7e43f534c40bc6843016
vvthinh <vvthinh1545@gmail.com>
2017-07-06 15:10:12 +0700

    list danh sách answers from question

--- app/src/renderer/components/Admin/QuestionList.vue
+++ app/src/renderer/components/Admin/QuestionList.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="ui main text container">
+    <button class="ui button primary" v-on:click="addAnswerByQuestion()">Tạo mới</button>
+    <button class="ui button primary" v-on:click="updateAnswer()">Lưu</button>
+    <div id="updateQuestion" class="ui modal">
+      <i class="close icon"></i>
+      <div class="header">
+        <h1>List Answers For Questions</h1>
+      </div>
+      <div class="ui content">
+
+        <table class="ui table segment">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Content</th>
+              <th>Is Correct</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="answer in answers">
+              <td>{{answer.name}}</td>
+              <td><textarea v-model="answer.content"></textarea></td>
+              <td><input type="checkbox" v-model="answer.isCorrect"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
     <table id="questions" class="display dataTable" cellspacing="0" width="100%">
       <thead>
@@ -28,22 +55,34 @@
   require('datatables.net')(window, $);
 
   export default {
+    data() {
+      return {
+
+        answers: [],
+        itemQuestion: {}
+      }
+    },
+
     computed: {
       ...mapState('adminQuestions', {
-        questions: state => state.all
 
+      }),
+      ...mapGetters('adminQuestions', {
+        questions: 'questions'
       })
     },
 
     created: co(function* () {
 
+      let me = this
+
       this.$store.dispatch('adminQuestions/getAll').then(() => {
 
         $(document).ready(() => {
 
-          let me = this
+
           let table = $('#questions').DataTable({
-            data: me.questions,
+            data: _.clone(me.questions),
 
             "columns": [{
                 "data": "id"
@@ -66,14 +105,40 @@
             }]
           })
           $('#questions tbody').on('click', 'tr', function () {
-            var data = table.row(this).data();
-            //  console.log('data', data.answersForAQuestions[0], data.id, data);
-            //alert(data.answersForAQuestions[0])
+            console.log('data', JSON.stringify(table.row(this).data().answersForAQuestions))
+            me.itemQuestion = table.row(this).data();
+            me.answers = table.row(this).data().answersForAQuestions.map(x => {
+              return _.clone(x)
+            })
+            let postQuestion = {};
+            postQuestion.content = me.itemQuestion.content;
+            postQuestion.description = me.itemQuestion.description;
+            postQuestion.id = me.itemQuestion.id;
+            postQuestion.quizId = me.itemQuestion.quizId;
+            postQuestion.type = me.itemQuestion.type;
+            postQuestion.answersForAQuestions = Object.assign({}, me.answers);
+            // console.log('postQuestion', postQuestion)
+            $('#updateQuestion').modal('show');
+
           });
         })
       })
 
-    })
+    }),
+    methods: function (answers) {
+      // handleName() {
+
+      //   let listName = ['A', 'B', 'C', 'D', 'E', 'G', 'H']
+      //   let getNames = listName.splice(0, answers.length - 1)
+      //   let ret = _.zipWith(getNames, answers, function (a, b) {
+      //     return Object.assign({}, a, {
+      //       name: b
+      //     })
+      //   })
+
+      // }
+
+    }
 
   }
 </script>
