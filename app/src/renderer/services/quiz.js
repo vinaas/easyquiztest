60a774e8efc9b008e9a9c658d0ae2770f8b3ad52
Phan Tấn Hiệp <hiepelpulga96@gmail.com>
2017-10-12 15:50:06 +0700

    thêm quizStatus= "Chưa thi" ;; quizStatus= "Đã thi"

--- app/src/renderer/services/quiz.js
+++ app/src/renderer/services/quiz.js
@@ -2,14 +2,22 @@ import Promise from 'bluebird'
 const pathEntity = '/api/Quizzes'
 export class QuizService {
   getAll = Promise.coroutine(function* (filter) {
+    console.log("SSS")
     let ret = yield axios.get(pathEntity)
     return ret.data
   })
   getBy = Promise.coroutine(function* (id) {
+    console.log("id_123", id)
     let ret = yield axios.get(`${pathEntity}/${id}`)
-    return ret.data
+    let abc = ret.data
+    abc.quizStatus = "Chưa thi"
+    abc.quizStatus = "Đã thi"
+    this.save(abc)    
+    // yield axios.post(`${pathEntity}/replaceOrCreate`, abc)
+    return abc
   })
   save = Promise.coroutine(function* (bindingEntity) {
+    console.log("quiz.js")
     let ret = yield axios.post(`${pathEntity}/replaceOrCreate`, bindingEntity)
     return ret.data
   })
