892c374eac191e66c39bc6b2c9a09775db202916
quocbao_home <quocbao.tn@gmail.com>
2017-10-25 16:15:22 +0700

    bổ sung questionType vào câu hỏi

--- backend/server/boot/scripts.js
+++ backend/server/boot/scripts.js
@@ -4,10 +4,12 @@ module.exports = function (app) {
     var RoleMapping = app.models.RoleMapping;
     var Question = app.models.Question;
     var Team = app.models.Team;
-    
+    var Quiz = app.models.Quiz;
+    var UserQuiz = app.models.UsersQuizs;
     User.create([
         { username: 'admin', email: 'admin@vinaas.com', password: '123' },
-        { username: 'user', email: 'user@vinaas.com', password: '123' },
+        { username: 'user01', email: 'user@vinaas.com', password: '123' },
+        { username: 'user02', email: 'user02@vinaas.com', password: '123' },
      ], function (err, users) {
         //create the admin role
         Role.create({
@@ -30,196 +32,47 @@ module.exports = function (app) {
             });
         });
     });
-    Question.create([
-        {
-            description: 'Nội dung câu hỏi 1',
-              type: '',
-              quizId: 0,
-              DifficultLevel : 5,
-              category : [],
-              isRandom : true
-        }
-    ]);
+    Quiz.create([
+        { name : "kì thi thứ 1", startDate: "2017/10/10",  quizTime: "2017/10/10",
+          quizStatus: "ACTIVE", totalTime : 30, numberOfQuestions: 20
+        },
+        { name : "kì thi thứ 2", startDate: "2017/09/10",  quizTime: "2017/10/10",
+        quizStatus: "ACTIVE", totalTime : 30, numberOfQuestions: 20
+      }
+    ], function (err, quiz) {
+        Question.create([
+            {
+                  Description: 'Nội dung câu hỏi 1, kì thi 1',
+                  quizId: quiz[0].id,
+                  DifficultLevel : 5,
+                  category : [],
+                  IsRandom : true,
+                  QuestionType: "One",
+                  ListAnswers : [
+                      { Content : "đáp án A", IsCorrect : "true" },
+                      { Content : "đáp án B"}
+                  ]
+            },
+            {
+                Description: 'Nội dung câu hỏi 2, kì thi 1',
+                quizId:  quiz[0].id,
+                DifficultLevel : 5,
+                category : [],
+                IsRandom : true,
+                QuestionType: "Multiple",
+                ListAnswers : [
+                    { Content : "đáp án A", IsCorrect : "true" },
+                    { Content : "đáp án B"},
+                    { Content : "đáp án C", IsCorrect : "true"}
+                ]
+          }
+        ]);
 
-    // PhongBan.create([
-
-    //     {
-    //         "ten": "Phòng Hành chính nhân sự",
-    //         "ma": "HAD",
-    //         "truongPhongUserId": null,
-    //     },
-    //     {
-    //         "ten": "Phòng Kinh doanh",
-    //         "ma": "SCD",
-    //         "truongPhongUserId": null,
-    //     },
-    //     {
-    //         "ten": "Phòng Năng lượng",
-    //         "ma": "EID",
-    //         "truongPhongUserId": null,
-    //     },
-    //     {
-    //         "ten": "Phòng Hàng hải - Tài sản - Kỹ thuật",
-    //         "ma": "MTI",
-    //         "truongPhongUserId": null,
-    //     },
-    //     {
-    //         "ten": "Chi nhánh Miền Bắc",
-    //         "ma": "NOB",
-    //         "truongPhongUserId": null,
-    //     },
-    //     {
-    //         "ten": "Chi nhánh Miền Trung",
-    //         "ma": "CEB",
-    //         "truongPhongUserId": null,
-    //     },
-    // ]);
-
-    // NghiepVu.create([
-    //     {
-    //         "ten": "Dầu thô",
-    //         "ma": "GDCL",
-    //         "soChungThu": "11",
-    //         "ngayTao": "2017-09-10T15:03:35.074Z",
-    //         "trangThai": true,
-    //         "phongBanId": 3,
-    //     },
-    //     {
-    //         "ten": "Gas",
-    //         "ma": "QDSL",
-    //         "soChungThu": "14",
-    //         "ngayTao": "2017-09-10T15:07:13.973Z",
-    //         "trangThai": true,
-    //         "phongBanId": 3,
-    //     },
-    //     {
-    //         "ten": "Dau Khi",
-    //         "ma": "QDSS",
-    //         "soChungThu": "21",
-    //         "ngayTao": "2017-09-25T15:18:42.987Z",
-    //         "trangThai": true,
-    //         "phongBanId": 3,
-    //     },
-    // ]);
+        UserQuiz.create([
+            { quizId : quiz[0].id, applicationUserId: 2 },
+            { quizId : quiz[1].id, applicationUserId: 3 }
+        ])
+    } );
     
-    // KhachHang.create([
-    //     {
-    //         "ma": "C00001",
-    //         "hoTen": "Phạm Thanh Tùng",
-    //         "diaChi": "123 Gò Vấp - Tp. HCM",
-    //         "email": "tung@mail.com",
-    //         "fax": "0301430045AQWE",
-    //         "soDienThoai": "0987654321",
-    //         "soTaiKhoan": "ABCD-12345",
-    //         "taiNganHang": "VCB",
-    //         "maSoThue": "MST-1234",
-    //         "soHopDong": "SHD-123",
-    //         "ghiChu": "Khách hàng bình thường",
-    //     },
-    //     {
-    //         "ma": "C00002",
-    //         "hoTen": "Huỳnh Thanh Danh",
-    //         "diaChi": "108 Cao Thắng, P4. Q3",
-    //         "email": "danhth90@gmail.com",
-    //         "fax": "097425635",
-    //         "soDienThoai": "0974205615",
-    //         "soTaiKhoan": "1253 3698 3695 1475",
-    //         "taiNganHang": "BIDV",
-    //         "maSoThue": "15792146",
-    //         "soHopDong": "CFC002",
-    //         "ghiChu": "Ghi Chú",
-    //     },
-    //     {
-    //         "ma": "C00003",
-    //         "hoTen": "Nguyễn Thị Hoài Thương",
-    //         "diaChi": "25 Hòa Đồng, Ngọc Định, Đồng Nai",
-    //         "email": "nththuong@gmail.com",
-    //         "fax": "091209210923",
-    //         "soDienThoai": "0980 192123",
-    //         "soTaiKhoan": "0910 1238 1239 3910",
-    //         "taiNganHang": "City Bank",
-    //         "maSoThue": "102835913",
-    //         "soHopDong": "CFC002",
-    //         "ghiChu": "Ghi Chú",
-    //     },
-    //     {
-    //         "ma": "C00003",
-    //         "hoTen": "Nguyễn Trọng Hiếu",
-    //         "diaChi": "25B, P. Tân Phú, TP HCM",
-    //         "email": "nthieu@gmail.com",
-    //         "fax": "0910129123",
-    //         "soDienThoai": "0983109210",
-    //         "soTaiKhoan": "0129 1210 2347 1998",
-    //         "taiNganHang": "BIDV",
-    //         "maSoThue": null,
-    //         "soHopDong": null,
-    //         "ghiChu": null,
-    //     },
-    //     {
-    //         "ma": "C00004",
-    //         "hoTen": "Nguyễn Thương Tín",
-    //         "diaChi": null,
-    //         "email": "nttin@gmail.com",
-    //         "fax": null,
-    //         "soDienThoai": null,
-    //         "soTaiKhoan": null,
-    //         "taiNganHang": null,
-    //         "maSoThue": null,
-    //         "soHopDong": null,
-    //         "ghiChu": null,
-    //     },
-    //     {
-    //         "ma": "C00005",
-    //         "hoTen": "Trần Đại Nghĩa",
-    //         "diaChi": "",
-    //         "email": null,
-    //         "fax": null,
-    //         "soDienThoai": null,
-    //         "soTaiKhoan": null,
-    //         "taiNganHang": null,
-    //         "maSoThue": null,
-    //         "soHopDong": null,
-    //         "ghiChu": null,
-    //     },
-    //     {
-    //         "ma": "C00006",
-    //         "hoTen": "Trương Nam Thành",
-    //         "diaChi": null,
-    //         "email": null,
-    //         "fax": null,
-    //         "soDienThoai": null,
-    //         "soTaiKhoan": null,
-    //         "taiNganHang": null,
-    //         "maSoThue": null,
-    //         "soHopDong": null,
-    //         "ghiChu": null,
-    //     },
-    //     {
-    //         "ma": "C00008",
-    //         "hoTen": "Trương Thị Thâm",
-    //         "diaChi": null,
-    //         "email": null,
-    //         "fax": null,
-    //         "soDienThoai": null,
-    //         "soTaiKhoan": null,
-    //         "taiNganHang": null,
-    //         "maSoThue": null,
-    //         "soHopDong": null,
-    //         "ghiChu": null,
-    //     },
-    //     {
-    //         "ma": "C00009",
-    //         "hoTen": "Tên Khách",
-    //         "diaChi": null,
-    //         "email": null,
-    //         "fax": null,
-    //         "soDienThoai": null,
-    //         "soTaiKhoan": null,
-    //         "taiNganHang": null,
-    //         "maSoThue": null,
-    //         "soHopDong": null,
-    //         "ghiChu": null,
-    //     },
-    // ]);
 };
 
