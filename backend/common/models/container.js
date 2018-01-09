76d4a3575da93dff8c36183fbf1b813abbea947e
quocbao_home <quocbao.tn@gmail.com>
2018-01-09 22:14:58 +0700

    chuyển sang upload excel sau đó parse dùng /api/Containers/parse

--- backend/common/models/container.js
+++ backend/common/models/container.js
@@ -1,5 +1,41 @@
 'use strict';
 
-module.exports = function(Container) {
+//Sử dụng https://github.com/vinaas/js-xlsx/
+// tham khảo từ : https://github.com/vinaas/js-xlsx/blob/master/demos/server/express.js#L1
+var fs = require('fs'), path = require('path'), URL = require('url');
+var express = require('express'), app = express();
+// var sprintf = require('printj').sprintf;
+// var logit = require('./_logit');
+// var cors = require('./_cors');
+var data = "a,b,c\n1,2,3".split("\n").map(function (x) { return x.split(","); });
+var XLSX = require('xlsx');
 
+function load_data(file) {
+    var wb = XLSX.readFile(file);
+    /* generate array of arrays */
+    data = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], { header: 1 });
+    return data;
+}
+
+module.exports = function (Container) {
+    Container.parse = function (fileName, cb) {
+        // post_data(req, res)    
+        var path = "./server/storage";
+        var file = path + "/xlsx/" + fileName; 
+        console.log('fileName', file)
+        
+        if (fs.existsSync(file)) {
+            var jsonData = load_data(file);
+            cb(null, jsonData);
+        }else {
+            var error = new Error("File not  found!");
+            error.status = 404;
+            cb(error);    
+        }
+    }
+
+    Container.remoteMethod('parse', {
+        accepts: { arg: 'fileName', type: 'string' },
+        returns: { arg: 'data', type: 'string' }
+    });
 };
--- backend/server/boot/routes.js
+++ backend/server/boot/routes.js
@@ -1,31 +1,5 @@
 'use strict';
 
-
-//Sử dụng https://github.com/vinaas/js-xlsx/
-// tham khảo từ : https://github.com/vinaas/js-xlsx/blob/master/demos/server/express.js#L1
-var fs = require('fs'), path = require('path'), URL = require('url');
-var express = require('express'), app = express();
-// var sprintf = require('printj').sprintf;
-// var logit = require('./_logit');
-// var cors = require('./_cors');
-var data = "a,b,c\n1,2,3".split("\n").map(function (x) { return x.split(","); });
-var XLSX = require('xlsx');
-
-function load_data(file) {
-  var wb = XLSX.readFile(file);
-  /* generate array of arrays */
-  data = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], { header: 1 });
-  return data;
-}
-
-function post_data(req, res) {
-  var keys = Object.keys(req.files), k = keys[0];
-  console.log('post_data', keys)
-  var jsonData = load_data(req.files[k].path);
-  res.send(jsonData);
-  // res.status(200).send(jsonData);
-}
-
 module.exports = function (app) {
   
   
@@ -33,11 +7,6 @@ module.exports = function (app) {
   app.get('/ping', function (req, res) {
     res.send('pong');
   });
-  app.use(require('express-formidable')());
-  app.post('/parse', function (req, res, next) {
-    // var url = URL.parse(req.url, true);
-    //   if(url.query.f) return post_file(req, res, url.query.f);
-    return post_data(req, res);
-  });
+ 
 };
 
