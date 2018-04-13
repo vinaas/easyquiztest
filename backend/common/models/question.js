f1a048ff8fddd18168d70939c790b049aab68d90
quocbao_home <quocbao.tn@gmail.com>
2018-04-13 09:29:19 +0700

    bổ sung thêm hạn chế truy cập nếu chưa đăng nhập

--- backend/common/models/question.js
+++ backend/common/models/question.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function (Question) {
-
+  Question.beforeRemote('find', function (ctx, instance, next) {
+    if (!ctx.args.filter || !ctx.args.filter.limit) {
+      console.log('forcing limit!');
+      if (!ctx.args.filter) ctx.args.filter = {};
+      ctx.args.filter.limit = 10;
+    }else if (ctx.args.filter.limit > 50) {
+      ctx.args.filter.limit = 50;
+    }
+    next();
+  });
 };
